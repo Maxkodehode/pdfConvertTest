f4b68b38845654eaddecb28e58a673968d5b031c
Max <maxkodehode@gmail.com>
2026-03-04 11:09:14 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using pdfTest;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace pdfTest;
+
+class Program
+{
+    static void Main()
+    {
+        var extract = new DocExtracter();
+        extract.extracter();
+    }
+}
